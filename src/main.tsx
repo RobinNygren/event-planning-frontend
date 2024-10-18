968d6fc4ad8a92f9786119fd0d214032fc21e750
biggieliten <filipisaksson00@gmail.com>
2024-10-18 10:54:11 +0200

    added react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { FrontPage } from "./routes/FrontPage";
+import { EventPage } from "./routes/EventPage";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FrontPage />,
+    children: [
+      {
+        path: "event-page", //dynamisk länk baserat på eventnamn? useParams?
+        element: <EventPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/routes/FrontPage.tsx
@@ -0,0 +1,7 @@
+export const FrontPage = () => {
+  return (
+    <>
+      <div>EventMaker20000000000</div>
+    </>
+  );
+};
