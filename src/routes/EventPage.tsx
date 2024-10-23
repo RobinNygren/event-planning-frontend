afedacb0fcc136c776b30356e0a4daeb3c637ff6
biggieliten <filipisaksson00@gmail.com>
2024-10-23 12:14:14 +0200

    commit

--- src/routes/EventPage.tsx
+++ src/routes/EventPage.tsx
@@ -1,17 +1,5 @@
 // import { EventCard } from "../components/EventCard";
 
 export const EventPage = () => {
-  const event = {
-    title: "Annual Tech Conference",
-    date: "March 25, 2024",
-    location: "San Francisco, CA",
-    description:
-      "Join us for a day of insightful talks and networking with industry leaders.",
-    imageUrl: "https://via.placeholder.com/400x200",
-  };
-  return (
-    <>
-      <EventCard {...event} />
-    </>
-  );
+  return <div className="eventpage">Event Page</div>;
 };
